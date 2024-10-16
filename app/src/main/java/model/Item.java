67930c09b8645d137f529180da69e2148fe05471
Sadek Alashkar <sa225sd@student.lnu.se>
2024-10-16 17:06:26 +0200

    some encapsulation

--- app/src/main/java/model/Item.java
+++ app/src/main/java/model/Item.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.checkerframework.checker.units.qual.m;
+
 public class Item {
   private String itemId;
   private String category;
@@ -12,7 +14,7 @@ public class Item {
   private LocalDate creationDate;
   private int costPerDay;
   private Member owner;
-  private List<Contract> lendingHistory;
+  private List<Contract> lendingHistory = new ArrayList<>();
   private boolean isAvailable;
 
 
@@ -25,8 +27,7 @@ public class Item {
     setCreationDate(creationDate);
     setCostPerDay(costPerDay);
     setOwner(owner);
-    setAvailable(isAvailable);
-    this.lendingHistory = new ArrayList<>();
+    markAsAvailable(); 
     
   }
   public Item(String category, String name, String description, int costPerDay, Member owner, LocalDate creationDate, String itemId) {
@@ -37,41 +38,38 @@ public class Item {
     setCreationDate(creationDate);
     setCostPerDay(costPerDay);
     setOwner(owner);
-    setAvailable(isAvailable);
-    this.lendingHistory = new ArrayList<>();
-
+    markAsAvailable();
   }
+
   private void setCreationDate(LocalDate creationDate) {
    this.creationDate = creationDate;
   }
-  public void setItemId(String itemId) {
+
+  private void setItemId(String itemId) {
     if (itemId == null){
       itemId = "ITEM" + System.nanoTime() % 1000000;
     }else this.itemId = itemId;
-     
-  }
-  public void setCategory(String category) {
-    this.category = category;
-  }
-  public void setName(String name) {
-    this.name = name;
-  }
-  public void setDescription(String description) {
-    this.description = description;
   }
 
+  private void setCategory(String category) {
+    this.category = category;
+  }
+
+  private void setName(String name) {
+    this.name = name;
+  }
+
+  private void setDescription(String description) {
+    this.description = description;
+  }
   
-  public void setCostPerDay(int costPerDay) {
+  private void setCostPerDay(int costPerDay) {
     this.costPerDay = costPerDay;
   }
-  public void setOwner(Member owner) {
+  private void setOwner(Member owner) {
     this.owner = owner;
   }
- 
-  public void setAvailable(boolean isAvailable) {
-    this.isAvailable = true;
-  }
-  // Getters
+
   public String getItemId() {
     return itemId;
   }

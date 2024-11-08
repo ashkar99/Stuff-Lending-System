a6fac6fd1eebada361abb09b8b84b7f0a906e49d
Sadek Alashkar <sa225sd@student.lnu.se>
2024-11-08 17:22:55 +0100

    Added SystemHandler to avoid member list in controller package

---
+++ app/src/main/java/model/SystemHandler.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Class that store and handle the members.
+ */
+public class SystemHandler {
+    private List<Member> members = new ArrayList<>();
+
+    public SystemHandler() {
+
+    }
+}

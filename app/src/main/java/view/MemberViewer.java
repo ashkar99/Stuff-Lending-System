33262a59a6b30797152400c6b8f4a36056b2623d
Zyad Dabbagh <zd222ay@student.lnu.se>
2024-11-12 17:33:44 +0100

    s

--- app/src/main/java/view/MemberViewer.java
+++ app/src/main/java/view/MemberViewer.java
@@ -7,84 +7,82 @@ import model.Member;
 import model.MemberRepository;
 
 /**
- * Handles member-related actions like creating, editing, deleting members, and displaying member information.
+ * Handles member-related actions like creating, editing, deleting members, and
+ * displaying member information.
  */
 public class MemberViewer extends BaseViewer {
-  private MemberRepository memberRepository = new MemberRepository();
-  private final MemberDaoInterface memberDao;
+
   private final ItemViewer itemViewer;
 
-  public MemberViewer(MemberDaoInterface memberDao) {
-    this.memberDao = memberDao;
-    this.memberDao.generated(); // Generate initial data if needed
+  public MemberViewer() {
     this.itemViewer = new ItemViewer();
   }
 
   /**
    * Allows a new user to sign in by providing their personal details: name,
-   * email, password, and phone number. The method adds the new member to the system.
+   * email, password, and phone number. The method adds the new member to the
+   * system.
    */
   public String[] createMember() {
     String name = promptForInput("Enter your name: ");
     String email = promptForInput("Enter your email: ");
     String password = promptForInput("Enter your password: ");
     String phoneNumber = promptForInput("Enter your phone number: ");
-
-    String[] memberInfo = {name, email, password, phoneNumber};
+    String [] member = {name,email,password,phoneNumber};
     System.out.println(FeedbackMessage.SUCCESS_MEMBER_CREATION.getMessage());
     waitForUserInput();
-    return memberInfo;
+    return member;
   }
 
   /**
-   * Allows the user to edit member information such as name, email, phone number, or password.
+   * Allows the user to edit member information such as name, email, phone number,
+   * or password.
    */
-  public void editMemberInfo() {
+  public String [] editMemberInfo() {
     String memberId = promptForInput("Enter member ID: ");
     String name = promptForInput("Enter new name: ");
     String email = promptForInput("Enter new email: ");
     String password = promptForInput("Enter new password: ");
     String phoneNumber = promptForInput("Enter new phone number: ");
+    String [] member = {memberId,name,email,password,phoneNumber};
 
-    memberDao.modifyMember(memberId, name, email, phoneNumber, password);
     System.out.println(FeedbackMessage.SUCCESS_MEMBER_UPDATE.getMessage());
     waitForUserInput();
+    return member;
   }
 
   /**
-   * Allows the user to delete a member based on the provided member ID and password.
+   * Allows the user to delete a member based on the provided member ID and
+   * password.
    */
-  public void deleteMember() {
+  public String [] deleteMember() {
     String memberId = promptForInput("Enter member ID: ");
     String password = promptForInput("Enter password: ");
-
-    memberDao.deleteMember(memberId, password);
+    String [] member = {memberId,password};
     System.out.println(FeedbackMessage.SUCCESS_MEMBER_DELETION.getMessage());
     waitForUserInput();
+    return member;
   }
 
   /**
    * Displays detailed information about a specific member based on the member ID.
    */
-  public void specificMemberFullInfo() {
+  public String specificMemberFullInfo() {
     displayMembersOverview();
     String memberId = promptForInput("Enter the member ID to view full information: ");
-    Member member = memberDao.showSpecificMemberInfo(memberId);
 
-    if (member != null) {
-      displayMemberInfo(member);
-    } else {
-      System.out.println(FeedbackMessage.ERROR_MEMBER_NOT_FOUND.getMessage());
-    }
+ 
+      //System.out.println(FeedbackMessage.ERROR_MEMBER_NOT_FOUND.getMessage());
+    
     waitForUserInput();
+    return memberId;
   }
 
   /**
-   * Displays a simple list of all members, showing only basic information like name, email, and phone number.
+   * Displays a simple list of all members, showing only basic information like
+   * name, email, and phone number.
    */
-  public void displayMembersOverview() {
-    List<Member> members = memberRepository.getMembers();
-
+  public void displayMembersOverview(List<Member> members) {
     if (members.isEmpty()) {
       System.out.println(FeedbackMessage.ERROR_NO_MEMBERS_TO_DISPLAY.getMessage());
     } else {
@@ -103,9 +101,7 @@ public class MemberViewer extends BaseViewer {
   /**
    * Displays all members along with their detailed items.
    */
-  public void displayMembersWithDetailedItems() {
-    List<Member> members = memberRepository.getMembers();
-
+  public void displayMembersWithDetailedItems(List<Member> members) {
     if (members.isEmpty()) {
       System.out.println(FeedbackMessage.ERROR_NO_MEMBERS_TO_DISPLAY.getMessage());
     } else {

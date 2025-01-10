2b54ce0c9a8141bd48e5363b473f56f8c831303e
Sadek Alashkar <sa225sd@student.lnu.se>
2025-01-10 21:18:12 +0100

    fix bugs and checkstyle

--- app/src/main/java/view/ContractViewer.java
+++ app/src/main/java/view/ContractViewer.java
@@ -1,10 +1,8 @@
 package view;
 
-
 import java.util.List;
 import model.ImmutableContract;
 import model.Item;
-
 
 /**
  * ContractViewer is responsible for managing contract-related interactions
@@ -16,13 +14,9 @@ public class ContractViewer extends BaseViewer {
   /**
    * Initializes a new ContractViewer with a reference to MemberDaoInterface
    * and creates an instance of ItemDaoImpl based on the given MemberDao.
-   *
-   * @param memberDao An instance of MemberDaoInterface to retrieve member
-   *                  information.
-   * @throws IllegalArgumentException if memberDao is null.
    */
   public ContractViewer() {
-   
+
   }
 
   /**
@@ -30,14 +24,14 @@ public class ContractViewer extends BaseViewer {
    * borrower's member IDs, item ID, and the start and end days for the contract.
    * Once validated, it initiates the contract creation process.
    */
-  public String [] createContract() {
+  public String[] createContract() {
     System.out.println("Creating a new contract...");
     String lenderId = promptForInput("Enter lender's member ID: ");
     String borrowerId = promptForInput("Enter borrower's member ID: ");
     String itemId = promptForInput("Enter item ID: ");
     String startDay = promptForInput("Enter start day (integer): ");
     String endDay = promptForInput("Enter end day (integer): ");
-    String [] contract = {lenderId, borrowerId,itemId,startDay,endDay};
+    String[] contract = { lenderId, borrowerId, itemId, startDay, endDay };
     return contract;
   }
 

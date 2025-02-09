cb22409942ba2c1779dd483ec13aca2a65364ab8
Sadek Alashkar <sa225sd@student.lnu.se>
2025-02-09 07:49:45 +0100

    manage invalid category input

--- app/src/main/java/model/CategoryEnum.java
+++ app/src/main/java/model/CategoryEnum.java
@@ -6,6 +6,13 @@ package model;
  * predefined categories.
  */
 public enum CategoryEnum {
-  TOOL, VEHICLE, GAME, TOY, SPORT, OTHER
+  TOOL, VEHICLE, GAME, TOY, SPORT, OTHER;
 
+  public static CategoryEnum safeCategoryParse(String categoryStr, CategoryEnum defaultCategory) {
+    try {
+      return !categoryStr.isBlank() ? CategoryEnum.valueOf(categoryStr) : defaultCategory;
+    } catch (IllegalArgumentException e) {
+      return defaultCategory;
+    }
+  }
 }

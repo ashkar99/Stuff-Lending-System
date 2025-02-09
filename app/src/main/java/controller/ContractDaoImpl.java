359639013028690bcf5ab7242efc7a15ff3bfbc1
Sadek Alashkar <sa225sd@student.lnu.se>
2025-02-09 11:58:37 +0100

    Re-implementation of Contract manage

--- app/src/main/java/controller/ContractDaoImpl.java
+++ app/src/main/java/controller/ContractDaoImpl.java
@@ -1,8 +1,5 @@
 package controller;
 
-import model.ImmutableContract;
-import model.Item;
-import model.Member;
 import model.SystemManager;
 import view.ContractViewer;
 import view.FeedbackMessage;
@@ -12,70 +9,14 @@ import view.FeedbackMessage;
  * of contracts.
  */
 public class ContractDaoImpl implements ContractDaoInterface {
-  private TimeDaoInterface timeDao = new TimeDaoImpl();
   private ContractViewer contractViewer = new ContractViewer();
   private SystemManager systemManager = new SystemManager();
 
   @Override
   public void createContract() {
 
-    String[] contractStrings = contractViewer.createContract();
-    Member lender = systemManager.getMemberById(contractStrings[0]);
-    Member borrower = systemManager.getMemberById(contractStrings[1]);
-
-    // Null check for lender and borrower
-    if (lender == null) {
-      throw new IllegalArgumentException(FeedbackMessage.ERROR_MEMBER_NOT_FOUND.getMessage());
-    }
-    if (borrower == null) {
-      throw new IllegalArgumentException(FeedbackMessage.ERROR_MEMBER_NOT_FOUND.getMessage());
-    }
-
-    // Fetch the item after verifying that the lender is not null
-    Item item = systemManager.getItemById(lender, contractStrings[2]);
-
-    if (item == null) {
-      throw new IllegalArgumentException(FeedbackMessage.ERROR_MEMBER_NOT_FOUND.getMessage());
-    }
-
-    int startDay = Integer.parseInt(contractStrings[3]);
-    int endDay = Integer.parseInt(contractStrings[4]);
-
-    int currentDay = timeDao.getCurrentDay(); // Get the current day from the TimeDao
-
-    // Check if the start day is in the past
-    if (startDay < currentDay) {
-      throw new IllegalArgumentException(FeedbackMessage.ERROR_CONTRACT_INVALID_TIME.getMessage());
-    }
-
-    // Check if the item is available for the given time period
-    if (!item.isAvailableForPeriod(startDay, endDay)) {
-      throw new IllegalArgumentException(FeedbackMessage.ERROR_ITEM_UNAVAILABLE.getMessage());
-    }
-
-    // Check if the borrower has enough funds
-    if (!isEnoughFundsToBorrow(borrower, item, startDay, endDay)) {
-      throw new IllegalArgumentException(FeedbackMessage.ERROR_INSUFFICIENT_CREDITS.getMessage());
-    }
-
-    // Create the contract and update the item and lender
-    ImmutableContract newContract = new ImmutableContract(lender, borrower, item, startDay, endDay);
-    borrower.updateCredits(-item.getCostPerDay() * (endDay - startDay + 1));
-    lender.updateCredits(item.getCostPerDay() * (endDay - startDay + 1));
-    item.addContract(newContract);
-    lender.addContract(newContract);
+    String[] contractInfo = contractViewer.createContract();
+    systemManager.createContract(contractInfo);
     contractViewer.displayFeedback(true, FeedbackMessage.SUCCESS_CONTRACT_CREATION.getMessage(), null);
-
-  }
-
-  @Override
-  public boolean isEnoughFundsToBorrow(Member borrower, Item item, int startDay, int endDay) {
-
-    int borrowerFunds = borrower.getCredits();
-    int itemTotalCost = item.getCostPerDay() * (endDay - startDay + 1);
-    if (borrowerFunds < itemTotalCost) {
-      throw new IllegalArgumentException(FeedbackMessage.ERROR_INSUFFICIENT_CREDITS.getMessage());
-    }
-    return true;
   }
 }

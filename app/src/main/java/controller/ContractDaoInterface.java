bc8239fd84c38824f6c20d9885cfae34a3192ce9
Sadek Alashkar <sa225sd@student.lnu.se>
2025-02-09 11:58:16 +0100

    Re-implementation of Contract manage

--- app/src/main/java/controller/ContractDaoInterface.java
+++ app/src/main/java/controller/ContractDaoInterface.java
@@ -1,8 +1,5 @@
 package controller;
 
-import model.Item;
-import model.Member;
-
 /**
  * Interface for managing contracts related to lending items between members.
  */
@@ -12,14 +9,4 @@ public interface ContractDaoInterface {
    * Creates a new contract between a lender and a borrower for a specific item.
    */
   void createContract();
-
-  /**
-   * Check if borrower have enough credits to lend the item.
-   *
-   * @param borrowe  borrower.
-   * @param item     item.
-   * @param startDay staartday.
-   * @param endDay   endadya.
-   */
-  boolean isEnoughFundsToBorrow(Member borrowe, Item item, int startDay, int endDay);
 }
